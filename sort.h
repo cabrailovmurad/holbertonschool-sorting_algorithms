87c6761a14e92cc8776799e47b56330163329b02
cabrailovmurad <dmurad908@gmail.com>
2025-08-10 18:15:16 +0000

    Add quick sort implementation and Big O notations

--- sort.h
+++ sort.h
@@ -14,9 +14,8 @@ typedef struct listint_s
 /* Given print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
