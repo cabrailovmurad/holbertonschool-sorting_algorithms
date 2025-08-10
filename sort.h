9893b63d9f1ec4521f8f3c2be8439c9f608996dc
cabrailovmurad <dmurad908@gmail.com>
2025-08-10 17:47:25 +0000

    Add selection sort implementation and time complexity file

--- sort.h
+++ sort.h
@@ -17,5 +17,6 @@ void print_list(const listint_t *list);
 
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
